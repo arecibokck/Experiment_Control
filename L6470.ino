c42dbf6bb5fb13f723878ee879b51627b90445d9
arecibokck <arecibokck@gmail.com>
2015-07-18 22:41:59 +0530

    Teensy Control Code of L6470 IC-based Stepper Motor Driver

--- L6470.ino
+++ L6470.ino
@@ -12,15 +12,17 @@
 #define speed_minus_button 22
 #define start_button= 23
 
-int t_max= 50; // number of steps
-int t_speed=111; //number of steps per second
-volatile int state1 = LOW;
-volatile int state2 = LOW;
-volatile int state3 = LOW;
-volatile int state4 = LOW;
-volatile int state5 = LOW;
-volatile int state6 = LOW;
-volatile int state7 = LOW;
+volatile int t_max= 50; // number of steps
+volatile int t_speed=111; //number of steps per second
+
+//initializes states of all buttons
+volatile int state_home_button = LOW;
+volatile int state_theta_max = LOW;
+volatile int state_minus_theta_button = LOW;
+volatile int state_plus_theta_button = LOW;
+volatile int state_speed_plus_button = LOW;
+volatile int state_speed_minus_button = LOW;
+volatile int state_start_button = LOW;
 
 LiquidCrystal lcd(0, 1, 2, 3, 4, 5); //sets pins lcd is connected to, running in 4 bit mode, RW pin tied to gnd
 L6470 stepper(10);
@@ -53,13 +55,13 @@ void setup(){
   
   stepper.init();
   stepper.setAcc(100); //set acceleration
-  stepper.setMaxSpeed(t_speed); 
-  stepper.setMinSpeed(1);
+  stepper.setMinSpeed(1); //sets minimum speed
   stepper.setMicroSteps(128); //1,2,4,8,16,32,64 or 128
   stepper.setThresholdSpeed(1000);
   stepper.setOverCurrent(6000); //set overcurrent protection
   stepper.setStallCurrent(3000);
   
+  //interrupts for each button
   attachInterrupt(home_button, interrupt_handler1, RISING);  
   attachInterrupt(theta_max_button, interrupt_handler2, RISING);
   attachInterrupt(minus_theta_button, interrupt_handler3, RISING);
@@ -72,57 +74,59 @@ void setup(){
 
 void loop(){  
   
-  if(state1 == HIGH){  //button pressed
+  stepper.setMaxSpeed(t_speed); //sets maximum speed
+  
+  if(state_home_button == HIGH){  //button pressed
     lcd.setCursor(0,0);  //clears display
     lcd.print("Set as Home");
-    stepper.setAsHome();
-    state1 == LOW
+    stepper.setAsHome(); //sets current position as Home
+    state_home_button == LOW
   }
   
-  if(state2== HIGH){  //button pressed
+  if(state_theta_max== HIGH){  //button pressed
     lcd.setCursor(0,0);  //clears display
     lcd.print("
     
-    state2 == LOW
+    state_theta_max == LOW
   }
   
-  if(state3== HIGH){  //button pressed
+  if(state_minus_theta_button== HIGH){  //button pressed
     lcd.setCursor(0,0);  //clears display
     lcd.print("Move Clockwise");
-    stepper.goTo(1);
-    state3 == LOW
+    stepper.move(1); //moves stepper through one step in the clockwise direction on each button press
+    state_minus_theta_button == LOW
   }
   
-  if(state4== HIGH){  //button pressed
+  if(state_plus_theta_button== HIGH){  //button pressed
     lcd.setCursor(0,0);  //clears display
     lcd.print("Move Counter-");
     lcd.print("Clockwise");
-    stepper.goTo(-1);
-    state4 == LOW
+    stepper.move(-1); //moves stepper through one step in the counter clockwise direction on each button press
+    state_plus_theta_button == LOW
   }
   
-  if(state5== HIGH){  //button pressed
+  if(state_speed_plus_button== HIGH){  //button pressed
     lcd.setCursor(0,0);  //clears display
-    lcd.print("Increase Speed");
-    stepper.goTo(-1);
-    state5 == LOW
+    lcd.print("Increased Speed");
+    t_speed = t_speed + 10 //Increases speed by 10 steps per second
+    state_speed_plus_button == LOW
   }
   
-  if(state6== HIGH){  //button pressed
+  if(state_speed_minus_button== HIGH){  //button pressed
     lcd.setCursor(0,0);  //clears display
-    lcd.print("Decrease Speed");
-    stepper.goTo(-1);
-    state6 == LOW
+    lcd.print("Decreased Speed");
+    t_speed = t_speed - 10 //Decreases speed by 10 steps per second
+    state_speed_minus_button == LOW
   }
   
-  if(state7== HIGH){  //button pressed
+  if(state_start_button== HIGH){  //button pressed to start motion
     cli();
     lcd.setCursor(0,0);  //clears display
     lcd.print("Start");
     while(1<2){
-      stepper.goTo(t_max);
-      stepper.goTo(-t_max);
-      if(state7 == LOW){  //button pressed
+      stepper.move(t_max); //moves by t_max in the clockwise direction
+      stepper.move(-t_max); //moves by t_max in the clockwise direction 
+      if(state_start_button == LOW){  //button pressed to stop motion
         lcd.setCursor(0,0);  //clears display
         lcd.print("Stop");
         break;
@@ -142,57 +146,70 @@ volatile unsigned long last_interrupt_time5 = 0;
 volatile unsigned long last_interrupt_time6 = 0;
 volatile unsigned long last_interrupt_time7 = 0;
 
+//handles interrupt for home button
 void interrupt_handler1(){
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time1 > 200){
-   state1 = HIGH;}
+   state_home_button = HIGH;}
   last_interrupt_time1 = interrupt_time;
 }
+
+//handles interrupt for t_max button
 void interrupt_handler2(){
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time2 > 200){
-   state2 = HIGH;}
+   state_theta_max = HIGH;}
   last_interrupt_time2 = interrupt_time;
 }
+
+//handles interrupt for plus_theta_button
 void interrupt_handler3(){
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time3 > 200){
-   state13= HIGH;}
+   state_home_button3= HIGH;}
   last_interrupt_time3 = interrupt_time;
 }
+
+//handles interrupt for minus_theta_button
 void interrupt_handler4(){
   
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time4 > 200){
-   state4 = HIGH;}
+   state_plus_theta_button = HIGH;}
   last_interrupt_time4 = interrupt_time;
 }
+
+//handles interrupt for speed_plus_button
 void interrupt_handler5(){
 
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time5 > 200){
-   state5 = HIGH;}
+   state_speed_plus_button = HIGH;}
   last_interrupt_time5 = interrupt_time;
 }
+
+//handles interrupt for speed_minus_button
 void interrupt_handler6(){
 
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time6 > 200){
-   state6 = HIGH;}
+   state_speed_minus_button = HIGH;}
   last_interrupt_time6 = interrupt_time;
 }
+
+//handles interrupt for start_button
 void interrupt_handler7(){
 
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time7 > 200){
-   state7 = HIGH;}
+   state_start_button = !state_start_button;}
   last_interrupt_time7 = interrupt_time;
 }
 
