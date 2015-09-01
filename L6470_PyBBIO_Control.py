7f8815b6e32e432529ee06718d042089d1140865
arecibokck <arecibokck@gmail.com>
2015-09-01 14:53:23 +0530

    BB Control Code of L6470 IC-based Stepper Motor Driver

---
+++ L6470_PyBBIO_Control.py
@@ -0,0 +1,36 @@
+import L6470_PyBBIO
+
+def setup():
+
+    speed = 100
+    acc = 2000
+    dec = 600
+    move = 90
+    motor = L6470()
+    print("Setup begun")
+
+    delay(1000)
+
+    print("Got motor config:")
+    print(motor.self.getConfig())
+
+    print("Got motor status:")
+    print(motor.self.getStatus())
+
+    motor.move(10000)
+    motor.setAcc(acc)
+    motor.setDec(dec)
+    motor.setMaxSpeed(speed)
+    motor.setMinSpeed(0)
+    print("\n\nSetup done")
+
+def loop():
+
+    delay(1)
+    motor.move(move)
+    print("\nMove+")
+    delay(1)
+    motor.move(-move)
+    print("Move-")
+
+run(setup, loop)
